b1f109822e0ab709bf15e96d268f42ae1fccecee
neonwednesdays <dxiao.ns@gmail.com>
2017-06-30 19:05:02 -0400

    splash page styles and css reset

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React, { Component } from 'react';
 
+/* pages */
+import Splash from './pages/Splash';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Splash />
       </div>
     );
   }
---
+++ src/pages/Splash.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import '../styles/splash.css';
+
+export default class App extends Component {
+	render() {
+		return (
+			<div className="sp-splash">
+				<h1 className="sp-splash--logo">SuperPerspective</h1>
+				<div className="sp-splash--inner">
+				</div>
+			</div>
+		);
+	}
+}
