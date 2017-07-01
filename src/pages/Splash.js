f3d026bdad43d823e3d4bb58a376bbfb4de07e8c
neonwednesdays <dxiao.ns@gmail.com>
2017-07-01 16:39:49 -0400

    use a transparent logo instead of just the icon

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -4,18 +4,10 @@ import '../styles/css/splash.css';
 import '../styles/css/mask-logo.css';
 
 export default class App extends Component {
-	static defaultProps = {
-		width: "100%",
-		height: "100%",
-	};
-
 	render() {
-		const { width, height } = this.props;
-
 		return (
 			<div className="sp-splash">
-				
-				<video className="sp-mask-logo--video" id="video" autoPlay="autoplay" muted="muted" preload="auto" loop="loop">
+				<video className="sp-mask-video" id="video" autoPlay="autoplay" muted="muted" preload="auto" loop="loop">
     				<source
     					src="http://www.elementten.com/media/sp-video-demo.mp4"
     					type="video/mp4"
