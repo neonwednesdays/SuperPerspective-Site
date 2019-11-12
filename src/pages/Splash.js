541fb52ba192fec7536906f57608bca5dddadf48
aahvocado <dan.dan.makes.stuff@gmail.com>
2019-11-12 13:24:55 -0800

    add Title on top of video in mobile

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -6,12 +6,16 @@ export default class App extends Component {
 	render() {
 		return (
 			<div className="sp-splash">
+        <div className='sp-title'>
+          Super Perspective
+        </div>
+
 				<video className="sp-mask-video" id="video" autoPlay="autoplay" muted="muted" preload="auto" loop="loop">
-    				<source
-    					src="http://www.elementten.com/media/sp-video-demo.mp4"
-    					type="video/mp4"
-					/>
-    			</video>
+  				<source
+  					src="http://www.elementten.com/media/sp-video-demo.mp4"
+  					type="video/mp4"
+				  />
+  			</video>
 			</div>
 		);
 	}
