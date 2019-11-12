f7ad83a76b71fb799519d7b35a3b44e56826a44b
aahvocado <dan.dan.makes.stuff@gmail.com>
2019-11-12 14:22:47 -0800

    add a developer list

--- src/App.js
+++ src/App.js
@@ -7,6 +7,12 @@ import Splash from './pages/Splash';
 /* components */
 import GetButton from './components/GetButton';
 
+function randomPrefix() {
+	const prefixList = ['Sir', 'The Dashing', 'Two-Dimensional', 'Three-Dimensional', 'The Amused', 'Crystalline', 'Glowing', 'Boxy', 'The Dapper', 'The Breezy', 'Create-dasher', 'Super'];
+	const randomIdx = Math.floor(Math.random() * (prefixList.length - 1));
+	return prefixList[randomIdx];
+}
+
 class App extends Component {
 	render() {
 		return (
@@ -25,6 +31,17 @@ class App extends Component {
 				<div className='sp-app--info'>
 					<div className='sp-app--get-container'>
 						<GetButton />
+					</div>
+
+					<div className='sp-app--dev-container'>
+						<h2 style={{fontSize: 14}}>Developed by</h2>
+						<ul className='dev-list'>
+							<li className='dev-item'>{`${randomPrefix()} Nick Shooter`}</li>
+							<li className='dev-item'>{`${randomPrefix()} Larry Smith`}</li>
+							<li className='dev-item'>{`${randomPrefix()} Peter Aquila`}</li>
+							<li className='dev-item'>{`${randomPrefix()} Arend Peter Castelein`}</li>
+							<li className='dev-item'>{`${randomPrefix()} Daniel Xiao`}</li>
+						</ul>
 					</div>
 				</div>
 			</div>
